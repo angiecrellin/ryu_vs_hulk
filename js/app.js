30691077dc6d3dcda8d558e8d01e464e748dec1b
angiecrellin <angiecrellin@gmail.com>
2016-06-26 13:13:19 -0500

    css/main.css

--- js/app.js
+++ js/app.js
@@ -3,16 +3,65 @@ $(document).ready(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
+
+
 		.mouseleave(function(){
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 		
 	})
+			
 		.mousedown(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
+			{'right': '1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('right', '520');
+			});
+	})	
+
+		.mouseup(function(){
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		
+	});
+
+		
+		$(document).keydown(function(e) {
+			if (e.keyCode == 88) {
+				$('.ryu-ready').hide();
+				$('.ryu-still').hide();
+				$('.ryu-cool').show();
+			}
+	})
+		
+	
+		.keyup(function(e) {
+			if(e.keyCode == 88) {
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+			}
+	});
+
+		
+		$('.ryu-hulk').mouseenter(function(){
+		$('.ryu-hulk-still').hide();
+		$('.ryu-hulk-ready').show();
+	})
+		.mouseleave(function(){
+		$('.ryu-hulk-ready').hide();
+		$('.ryu-hulk-still').show();
+		
+	})
+		.mousedown(function() {
+		playHadouken();
+		$('.ryu-hulk-ready').hide();
+		$('.ryu-hulk-throwing').show();
+		$('.hadouken-hulk').finish().show().animate(
 			{'left': '1020px'},
 			500,
 			function(){
@@ -22,24 +71,28 @@ $(document).ready(function(){
 	})	
 
 		.mouseup(function(){
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
+		$('.ryu-hulk-throwing').hide();
+		$('.ryu-hulk-ready').show();
 		
-	});
-		$(document).keydown(function(e) {
-			if (e.keyCode == 88) {
-				$('.ryu-ready').hide();
-				$('.ryu-still').hide();
-				$('.ryu-cool').show();
+	})
+	
+
+		$(document).keydown(function(f) {
+			if (f.keyCode == 89) {
+				$('.ryu-hulk-ready').hide();
+				$('.ryu-hulk-still').hide();
+				$('ryu-hulk-throwing').hide();
+				$('.ryu-hulk-cool').show();
 			}
 	})
-		.keyup(function(e) {
-			if(e.keyCode == 88) {
-				$('.ryu-cool').hide();
-				$('.ryu-still').show();
+
+		.keyup(function(f) {
+			if(f.keyCode == 89) {
+				$('.ryu-hulk-cool').hide();
+				$('.ryu-hulk-still').show();
+
 			}
 	});
-	
 
 });
 	function playHadouken() {
